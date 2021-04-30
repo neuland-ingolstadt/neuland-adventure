82c2565f467ca037c85de1ed140d32d4a1c22419
Schrottpage <39926202+Schrottpage@users.noreply.github.com>
2021-04-30 14:27:08 +0000

    Deploying to gh-pages from master @ 8dd831b906172a029b02b5368dce8b33feabca63 🚀

---
+++ Tiles/OpenGameArt/bench-backless-wood.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="bench-backless-wood" tilewidth="32" tileheight="32" tilecount="18" columns="3">
+ <image source="bench-backless-wood.png" width="96" height="192"/>
+</tileset>
