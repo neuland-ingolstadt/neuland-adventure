8dd831b906172a029b02b5368dce8b33feabca63
Quirin <schrottpage@users.noreply.github.com>
2021-04-30 16:26:12 +0200

    added some tilesets
    added some tilesets from OpenGameArt

---
+++ Tiles/OpenGameArt/trees-and-bushes.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="trees-and-bushes" tilewidth="32" tileheight="32" tilecount="45" columns="9">
+ <image source="trees-and-bushes.png" width="288" height="160"/>
+</tileset>
