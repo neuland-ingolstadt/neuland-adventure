f44ead9b87737d229124ce89f4a6d486862052d4
Alexander Horn <alexhorn@users.noreply.github.com>
2021-05-07 00:23:41 +0200

    Embed TSX tilesets

--- Tiles/OpenGameArt/bench-wood.tsx
+++ Tiles/OpenGameArt/bench-wood.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.5" tiledversion="1.5.0" name="bench-wood" tilewidth="32" tileheight="32" tilecount="8" columns="2">
- <image source="Tiles/OpenGameArt/bench-wood.png" width="64" height="128"/>
+ <image source="bench-wood.png" width="64" height="128"/>
 </tileset>
